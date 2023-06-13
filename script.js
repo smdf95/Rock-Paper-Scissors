3ff798be431a238cbb68b800591f0b0887344635
smdf95 <s.fisher95@hotmail.com>
2023-06-13 18:35:20 +0100

    copied code from other file in order to upload

--- script.js
+++ script.js
@@ -1,202 +1,166 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    var choices = ["rock", "paper", "scissors"];
+    var userScore = 0;
+    var computerScore = 0;
+  
     function rockPaperScissors(choice) {
-    
-        var choices = ["rock", "paper", "scissors"];
-    
-        if (!choices.includes(choice)) {
-            console.log("Invalid Choice!");
-            return;
-        }
-    
-        var computerChoice = computerWeapon();
-        var outcome = determineOutcome(choice, computerChoice);
-    
-        displayUserChoice(choice);
-        displayComputerChoice(computerChoice);
-    
-        setTimeout(function() {
-            removeChoices();
-            displayOutcomeImage(outcome, choice, computerChoice);
-        }, 3000);
-        
-    
-        
+      if (!choices.includes(choice)) {
+        console.log("Invalid Choice!");
+        return;
+      }
+
+      hideChoices();
+  
+      var computerChoice = computerWeapon();
+      var outcome = determineOutcome(choice, computerChoice);
+  
+      displayUserChoice(choice);
+      displayComputerChoice(computerChoice);
+  
+      setTimeout(function() {
+        removeChoices();
+        displayOutcomeImage(outcome, choice, computerChoice);
+        showReplayButton();
+      }, 2000);
     }
-    
-    
-    
+  
     function determineOutcome(userChoice, computerChoice) {
-        
-        
-        if (userChoice === computerChoice) {
-            return "tie";
-        } else if(
-            (userChoice === "rock" && computerChoice === "scissors") ||
-            (userChoice === "paper" && computerChoice === "rock") ||
-            (userChoice === "scissors" && computerChoice === "paper")
-        ) {
-            return "userWin";
-        } else {
-            return "computerWin";
-        }
-        
+      if (userChoice === computerChoice) {
+        return "tie";
+      } else if (
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "rock") ||
+        (userChoice === "scissors" && computerChoice === "paper")
+      ) {
+        return "userWin";
+      } else {
+        return "computerWin";
+      }
     }
-    
+  
     function displayUserChoice(choice) {
         var userChoiceElement = document.createElement("div");
-        userChoiceElement.textContent = "You chose " + choice;
-
+        
         var userChoiceImage = document.createElement("img");
         userChoiceImage.setAttribute("src", choice + ".png");
         userChoiceImage.setAttribute("alt", choice);
         userChoiceImage.setAttribute("height", "100px");
         userChoiceElement.appendChild(userChoiceImage);
-
+      
+        var userChoiceText = document.createElement("div");
+        userChoiceText.textContent = "You chose " + choice;
+        userChoiceElement.appendChild(userChoiceText);
+      
         document.getElementById("userChoice").appendChild(userChoiceElement);
-    }
-    
-    
-    function displayComputerChoice(choice) {
+      }
+      
+      function displayComputerChoice(choice) {
         var computerChoiceElement = document.createElement("div");
-        computerChoiceElement.textContent = "Computer chose " + choice;
-    
+        
         var computerChoiceImage = document.createElement("img");
         computerChoiceImage.setAttribute("src", choice + ".png");
         computerChoiceImage.setAttribute("alt", choice);
         computerChoiceImage.setAttribute("height", "100px");
         computerChoiceElement.appendChild(computerChoiceImage);
-    
+      
+        var computerChoiceText = document.createElement("div");
+        computerChoiceText.textContent = "Computer chose " + choice;
+        computerChoiceElement.appendChild(computerChoiceText);
+      
         document.getElementById("computerChoice").appendChild(computerChoiceElement);
       }
-    
-    function removeChoices() {
-        
-        var userChoiceElement = document.getElementById("userChoice");
-        
-        var computerChoiceElement = document.getElementById("computerChoice");
-        
-        while (userChoiceElement.firstChild) {
-            userChoiceElement.firstChild.remove();
-        }
-    
-        while (computerChoiceElement.firstChild) {
-            computerChoiceElement.firstChild.remove();
-        }
-    }
-    
-    
-    function displayOutcomeImage(outcome, userChoice, computerChoice) {
-        var imageElement = document.createElement("img");
-        var parentElement = document.getElementById("outcomeContainer");
-    
-        var imageSources = {
-            tie: userChoice + "Tie" + ".jpg",
-            userWin: userChoice + "Wins" + ".jpg",
-            computerWin: computerChoice + "Wins" + ".jpg"
-        };
-
-        
-    
-        if (imageSources.hasOwnProperty(outcome)) {
-            imageElement.setAttribute("src", imageSources[outcome]);
-            imageElement.setAttribute("alt", "Outcome");
-            imageElement.setAttribute("height", "100px")
-            parentElement.appendChild(imageElement);
-        } else {
-            console.log("Invalid Outcome")
-        }
-
-        outcomeElement.textContent = outcome; // Set the outcome text
-
-        // Example: "Rock wins!"
-        if (outcome === "userWin") {
-            outcomeElement.textContent = "You win with " + userChoice + "!";
-        }
-        if (outcome === "computerWin") {
-            outcomeElement.textContent = "Computer wins with " + computerChoice + "!";
-        }
-        if (outcome === "tie") {
-            outcomeElement.textContent = `It's a tie!`;
-        }
-        var playAgainButton = document.createElement("button");
-        playAgainButton.setAttribute("id", "playAgainButton");
-        playAgainButton.textContent = "Play Again";
-
-        // Add an event listener to the button
-        playAgainButton.addEventListener("click", function() {
-            resetGame();
-        });
-
-        // Append the button to the outcome container
-        var replayDiv = document.getElementById("replay");
-        replayDiv.appendChild(playAgainButton);
-        
-    }
-    
-    
-    function computerWeapon() {
-    
-        weapon = randomInteger(1, 3);
-    
-        switch (weapon) {
-    
-            case 1: 
-                console.log("Computer chose rock");
-                return "rock";
-                
-            case 2:
-                console.log("Computer chose paper");
-                return "paper";
-    
-            case 3:
-                console.log("Computer chose scissors");
-                return "scissors";
-    
-            default:
-                return "";
-    
-        }
-    }
-    
-    function randomInteger(low, high) {
-        return low + Math.floor((high - low + 1) * Math.random());
-    }
-    
-    var rockImage = document.getElementById("rock");
-    rockImage.addEventListener("click", function() {
-        var userChoice ="rock";
-        rockPaperScissors(userChoice);
-        rockImage.remove();
-        paperImage.remove();
-        scissorsImage.remove();
-    });
-    
-    
-    var paperImage = document.getElementById("paper");
-    paperImage.addEventListener("click", function() {
-        var userChoice ="paper";
-        rockPaperScissors(userChoice);
-        rockImage.remove();
-        paperImage.remove();
-        scissorsImage.remove();
-    });
-    
-    
-    
-    var scissorsImage = document.getElementById("scissors");
-    scissorsImage.addEventListener("click", function() {
-        var userChoice ="scissors";
-        rockPaperScissors(userChoice);
-        paperImage.remove();
-        rockImage.remove();
-        scissorsImage.remove();
-    });
-    
-    function resetGame() {
-        
-        window.location.href = "index.html";
-      }
       
-   
-    });
+
+    function hideChoices() {
+        var optionsDiv = document.getElementById("gameOptions");
+        optionsDiv.innerHTML = "";
+    }
+  
+    function removeChoices() {
+      var userChoiceElement = document.getElementById("userChoice");
+      var computerChoiceElement = document.getElementById("computerChoice");
+  
+      userChoiceElement.innerHTML = "";
+      computerChoiceElement.innerHTML = "";
+    }
+  
+    function displayOutcomeImage(outcome, userChoice, computerChoice) {
+      var outcomeElement = document.getElementById("outcomeElement");
+      var imageElement = document.createElement("img");
+      var parentElement = document.getElementById("outcomeContainer");
+  
+      var imageSources = {
+        tie: userChoice + "Tie" + ".jpg",
+        userWin: userChoice + "Wins" + ".jpg",
+        computerWin: computerChoice + "Wins" + ".jpg"
+      };
+  
+      if (imageSources.hasOwnProperty(outcome)) {
+        imageElement.setAttribute("src", imageSources[outcome]);
+        imageElement.setAttribute("alt", "Outcome");
+        imageElement.setAttribute("height", "100px");
+        parentElement.appendChild(imageElement);
+      } else {
+        console.log("Invalid Outcome");
+      }
+  
+      outcomeElement.textContent = outcome;
+  
+      if (outcome === "userWin") {
+        outcomeElement.textContent = "You win with " + userChoice + "!";
+        userScore++;
+        document.getElementById("userScore").textContent = userScore;
+      } else if (outcome === "computerWin") {
+        outcomeElement.textContent = "Computer wins with " + computerChoice + "!";
+        computerScore++;
+        document.getElementById("computerScore").textContent = computerScore;
+      } else if (outcome === "tie") {
+        outcomeElement.textContent = "It's a tie!";
+      }
+    }
+  
+    function showReplayButton() {
+      var replayDiv = document.getElementById("replay");
+      replayDiv.innerHTML = ""; // Clear the replay div
+  
+      var replayButton = document.createElement("button");
+      replayButton.setAttribute("id", "replayButton");
+      replayButton.textContent = "Play Again";
+      replayButton.addEventListener("click", function() {
+        replayDiv.innerHTML = ""; // Clear the replay div
+        document.getElementById("outcomeContainer").innerHTML = ""; // Clear the outcome image
+        document.getElementById("outcomeElement").textContent = ""; // Clear the outcome text
+        removeChoices(); // Remove the displayed choices
+        showGameOptions(); // Show the game options again
+      });
+  
+      replayDiv.appendChild(replayButton);
+    }
+  
+    function showGameOptions() {
+      var optionsDiv = document.getElementById("gameOptions");
+      optionsDiv.innerHTML = ""; // Clear the game options
+  
+      choices.forEach(function(choice) {
+        var choiceImage = document.createElement("img");
+        choiceImage.setAttribute("src", choice + ".png");
+        choiceImage.setAttribute("alt", choice);
+        choiceImage.setAttribute("id", choice);
+        choiceImage.setAttribute("height", "100px");
+        choiceImage.addEventListener("click", function() {
+          rockPaperScissors(choice);
+        });
+  
+        optionsDiv.appendChild(choiceImage);
+      });
+    }
+  
+    function computerWeapon() {
+      var randomIndex = Math.floor(Math.random() * choices.length);
+      return choices[randomIndex];
+    }
+  
+    showGameOptions(); // Initially show the game options
+  });
+  
